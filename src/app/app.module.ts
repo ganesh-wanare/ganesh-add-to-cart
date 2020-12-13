b06d83df0d4bc3978d7d33c30a4affcbde1c554e
ganesh-wanare <ganesh.wanare24@gmail.com>
2020-12-13 17:27:32 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,39 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule } from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HomeComponent } from './PAGES/home/home.component';
+import { CheckoutComponent } from './PAGES/checkout/checkout.component';
+
+import {HeaderComponent} from './COMPONENTS/header/header.component';
+import {MatCardModule} from '@angular/material/card';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductsComponent } from './COMPONENTS/products/products.component';
+import { CheckoutProductsComponent } from './COMPONENTS/checkout-products/checkout-products.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CheckoutComponent,
+    HeaderComponent,
+    ProductsComponent,
+    CheckoutProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
